1222d19d89298689acdc74fcb3a63799caa66a2f
konan0802 <konanforbis@gmail.com>
2025-07-27 00:09:58 +0900

    add greatest-common-divisor-of-stringsgreatest-common-divisor-of-strings

--- LeetCode75/greatest-common-divisor-of-strings.py
+++ LeetCode75/greatest-common-divisor-of-strings.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/greatest-common-divisor-of-strings/
+
+"""
+・ライブラリの読み込み時には利用するものだけを読み込む
+・`Brute force`ではなく、必ず数学的アプローチを利用する
+"""
+
+from math import gcd
+
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if str1 + str2 != str2 + str1:
+            return ""
+        gcd_len = gcd(len(str1), len(str2))
+        return str1[:gcd_len]
