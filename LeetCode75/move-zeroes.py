dae5115c7b425b2b3dc627d87a35093c22c5be1e
konan0802 <konanforbis@gmail.com>
2025-08-01 10:23:57 +0900

    add move-zeroes

---
+++ LeetCode75/move-zeroes.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/move-zeroes/
+
+"""
+・zeros = [] で別の配列を作成し、空間計算量がO(k)（kは0の個数）になっている
+・nums.pop() 操作は要素を削除する際に後続要素を前にシフトするため、1回の操作でO(n)の時間がかかる
+・nums.extend() 操作はO(k)の時間がかかる
+"""
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        i = 0
+        zeros = []
+        while length > i:
+            if nums[i] == 0:
+                zeros.append(nums.pop(i))
+                length-=1
+            else:
+                i += 1
+        nums.extend(zeros)
+
+class CorrectSolution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Two-pointer approach: O(n) time, O(1) space
+        """
+        j = 0
+        for num in nums:
+            if num != 0:
+                nums[j] = num
+                j += 1
+        for i in range(j, len(nums)):
+            nums[i] = 0
