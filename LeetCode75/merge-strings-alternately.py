1e85623b6788781c897ce41f12c388175297690a
konan0802 <konanforbis@gmail.com>
2025-08-01 11:17:40 +0900

    update merge-strings-alternately

--- LeetCode75/merge-strings-alternately.py
+++ LeetCode75/merge-strings-alternately.py
@@ -1,10 +1,18 @@
 # https://leetcode.com/problems/merge-strings-alternately/
 
 """
-・文字列をリストに変換するにはlist()を使用する
-・リストを文字列に変換するには''.join()を使用する
+解説：
+・Two-Pointerパターン：2つのポインタを使用して、2つの文字列を交互に結合する
+・時間計算量：O(n) - 2つの文字列を1回ずつ走査
+・空間計算量：O(1) - 追加のメモリ使用はなし
+
+問題点：
 ・リストに要素を挿入するにはinsert()を使用する
   ※挿入のたびに後ろの要素をずらすためコストが高く、全体で二乗時間になりやすい
+
+知見：
+・文字列をリストに変換するにはlist()を使用する
+・リストを文字列に変換するには''.join()を使用する
 ・リストの要素を取得するにはenumerate()を使用する
 """
 
@@ -16,3 +24,21 @@ class Solution:
             word_list.insert(insert_index, w2)
         output = ''.join(word_list)
         return output
+
+class CorrectSolution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        result = []
+        i = j = 0
+        
+        # 両方の文字列に文字が残っている間、交互に追加
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+        
+        # 残りの文字を追加
+        result.append(word1[i:])
+        result.append(word2[j:])
+        
+        return ''.join(result)
